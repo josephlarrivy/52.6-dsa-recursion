02ca6a1d451defa24328e236079a83bad87ac460
josephlarrivy <joseph.larrivy@gmail.com>
2023-02-11 15:58:42 -0600

    assignment complete

--- recursion.js
+++ recursion.js
@@ -4,20 +4,12 @@ function product(nums) {
   return nums[0] * product(nums.slice(1));
 }
 
-
-
-
-
-
-
-/** longest: return the length of the longest word in an array of words. */
-function longest(words) {
+// DONE /** longest: return the length of the longest word in an array of words. */
+function longest(words, length=0) {
+  if (words.length === 0) return length;
+  length = Math.max(words[0].length, length)
+  return longest(words.slice(1), length)
 }
-
-
-
-
-
 
 // DONE /** everyOther: return a string with every other letter. */
 function everyOther(str) {
@@ -25,18 +17,20 @@ function everyOther(str) {
   return str[0] + everyOther(str.slice(2))
 }
 
-
-
-
-
-/** isPalindrome: checks whether a string is a palindrome or not. */
-function isPalindrome(str) {
-
+// DONE /** isPalindrome: checks whether a string is a palindrome or not. */
+function isPalindrome(str, idx=0) {
+  let leftIdx = idx;
+  let rightIdx = str.length - idx - 1;
+  if (leftIdx >= rightIdx) return true;
+  if (str[leftIdx] !== str[rightIdx]) return false;
+  return isPalindrome(str, idx + 1);
 }
 
-/** findIndex: return the index of val in arr (or -1 if val is not present). */
-function findIndex(arr, val) {
-
+// DONE /** findIndex: return the index of val in arr (or -1 if val is not present). */
+function findIndex(arr, val, count=0) {
+  if (count == arr.length) return -1;
+  if (arr[count] == val) return count;
+  return findIndex(arr, val, count+1)
 }
 
 //DONE /** revString: return a copy of a string, but in reverse. */
@@ -45,37 +39,46 @@ function revString(str) {
   return str[str.length -1] + revString(str.slice(0, str.length-1));
 }
 
-
-
-
-
 /** gatherStrings: given an object, return an array of all of the string values. */
+// function gatherStrings(obj, arr=[]) {
+//   for (let o in obj) {
+//     if (typeof obj[o] == "string") arr.push(obj[o]);
+//     if (typeof obj[o] == "object") arr.push(...gatherStrings(obj[o], arr));
+//   }
+//   return arr;
+// }
+
+// couldn't figure this one out
 function gatherStrings(obj) {
-  for (let o in obj) {
-    if (typeof o == "object") {
-      gatherStrings(o);
-      console.log('test')
-    } else {
-      console.log(o);
-    }
+  let stringArr = [];
+  for (let key in obj) {
+    if (typeof obj[key] === "string") stringArr.push(obj[key]);
+    if (typeof obj[key] === "object") stringArr.push(...gatherStrings(obj[key]));
   }
+  return stringArr;
 }
-
-let nestedObj = {
-  firstName: "Lester",
-  favoriteNumber: 22,
-  moreData: {
-    lastName: "Testowitz"
-  }
-};
-
-console.log(gatherStrings(nestedObj))
 
 /** binarySearch: given a sorted array of numbers, and a value,
  * return the index of that value (or -1 if val is not present). */
-function binarySearch(arr, val) {
+// function binarySearch(arr, val, count=0) {
+//   if (arr[0] == val) return count;
+//   if (arr.length == 0) return -1;
+//   return binarySearch(arr.slice(1), val, count+1)
+// }
 
+// gave this one a shot. couldn't quite get it the most efficient way
+
+function binarySearch(arr, val, leftIdx=0, rightIdx=arr.length) {
+  if (leftIdx>rightIdx) return -1;
+
+  let middleIdx = Math.floor((rightIdx + leftIdx) / 2);
+
+  if (arr[middleIdx] === val) return middleIdx;
+  if (arr[middleIdx] > val) return binarySearch(arr, val, leftIdx, middleIdx - 1);
+
+  return binarySearch(arr, val, middleIdx + 1, rightIdx);
 }
+
 
 module.exports = {
   product,
